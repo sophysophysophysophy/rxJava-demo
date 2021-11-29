eeea0a8a9c5de9ba49a40d943e9182400bfc433d
정현주 <sophy@hyundai-autoever.com>
2021-11-29 17:53:51 +0900

    환경 세팅 테스트

---
+++ src/main/java/com/sophy/HelloRxJava.java
@@ -0,0 +1,12 @@
+package com.sophy;
+
+
+import io.reactivex.Observable;
+
+public class HelloRxJava {
+    public static void main(String[] args) { //psvm : live template
+        Observable<String> observable = Observable.just("Hello", "RxJava");
+        observable.subscribe(data -> System.out.println(data));
+
+    }
+}
